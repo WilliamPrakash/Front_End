3a6ef274cc54680f553ed848438c766f49003863
WilliamPrakash <willprakash@gmail.com>
2024-04-02 21:07:05 -0400

    added basic url routing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './css/index.css';
-import App from './App';
+import Home from './components/Home'
+import Page1 from './components/Page1'
 
-/* 
-  index.js typically handles your app startup, 
-  routing, and other functions of your application,
-  and also does require other modules to add functionality
-*/
+// index.js handles app startup, routing, and other functions
 
-ReactDOM.render(
+// these are the url paths
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="page1" element={<Page1 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-)
+)*/
 
