3da8b15cc90847ddc3f80aa3d9825ce064289a21
WilliamPrakash <willprakash@gmail.com>
2024-04-07 10:49:27 -0400

    successfully rendering page links thanks to w3schools

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './css/index.css';
+import Layout from './components/Layout'
 import Home from './components/Home'
 import Page1 from './components/Page1'
 
 // index.js handles app startup, routing, and other functions
 
-// these are the url paths
 export default function App() {
   return (
+    // BrowserRouter only handles URL routing. Without the Layout, you'd need to manually manipulate the URL
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="page1" element={<Page1 />} />
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home />} />
+          <Route path='page1' element={<Page1 />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
