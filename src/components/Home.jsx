d628bcef35a23a734851965f89f55494c1d3bca4
WilliamPrakash <willprakash@gmail.com>
2025-03-02 10:33:29 -0500

    Organizing Home.jsx's CSS

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ export default function home() {
             <h2>Inside Home.jsx</h2>
           </header>
           
-          <body className='flex, grid'>
+          <div className='flex grid'>
             <h3>Choose which block to scroll to:</h3>
             <ul>
               <li type='button' onClick="document.getElementById('box1').scrollIntoView()">Section 1</li>
@@ -19,21 +19,21 @@ export default function home() {
               <li>Section 4</li>
             </ul>
             <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, est doloremque. Maxime porro obcaecati laborum magnam dicta inventore aut repellendus corporis nostrum, necessitatibus iste magni? Veritatis obcaecati consequatur, laudantium porro fuga amet eveniet, omnis ad sed deserunt doloremque quidem necessitatibus beatae nemo non? Autem ad nihil voluptate optio reiciendis deserunt.</div>
-          </body>
+          </div>
 
-          <div className='flex, box1'>
+          <div className='flex box1'>
 
           </div>
 
-          <div className='flex, box2'>
+          <div className='flex box2'>
 
           </div>
 
-          <div className='flex, box3'>
+          <div className='flex box3'>
 
           </div>
 
-          <div className='flex, box4'>
+          <div className='flex box4'>
 
           </div>
 
