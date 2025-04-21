516460dcac65a613708fa2d3cddc990d2b7bab53
WilliamPrakash <willprakash@gmail.com>
2025-04-21 19:21:46 -0400

    Added basic API call to API_DotNet

--- src/components/Employees_ChildComponents/EmployeeList.jsx
+++ src/components/Employees_ChildComponents/EmployeeList.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Employee from './Employee.jsx'
+import $ from 'jquery'
 
 export default function EmployeeList() {
     /* API call(s) here?? */
-
+    $.get({
+        type: 'GET',
+        url: 'http://127.0.0.1:5215/api/Employees/GetEmployees',
+        dataType: 'JSON',
+        success: function(data) {
+            console.log(data)
+        }
+    })
+    
     const employees = [{
         Id: 998,
         Name: 'Jeff',
@@ -13,7 +22,7 @@ export default function EmployeeList() {
         Id: 999,
         Name: 'Christina',
         Email: 'christina@email.com',
-        Occupation: 'CEO of Christina Cnterprises'
+        Occupation: 'CEO of Christina Enterprises'
     }]
 
     return (
