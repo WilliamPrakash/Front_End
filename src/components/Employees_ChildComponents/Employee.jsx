87f7a49a0f3a421ebb7a1d58f675e19e00e55a83
WilliamPrakash <willprakash@gmail.com>
2025-04-24 13:17:57 -0400

    Added bare bones of edit employee popup modal

--- src/components/Employees_ChildComponents/Employee.jsx
+++ src/components/Employees_ChildComponents/Employee.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
+import EditEmployee from './EditEmployee.jsx'
 
 export default function Employee(props) {
+    const [edit, setEditStatus] = useState(false);
+
+    function toggleEditPopup() {
+        setEditStatus(edit => !edit);
+    };
 
     return (
         <li className='li_employee' key={props.employee.id}>
             <p>{ 'Id: ' + props.employee.id + ', Name: ' + props.employee.name + ', Email: ' + props.employee.email + ', Occupation: ' + props.employee.occupation }</p>
+            <button onClick={toggleEditPopup}>Edit {props.employee.name}</button>
+            {edit ? <EditEmployee props={props}/> : null}
         </li>
-    )
+    );
 }
--- src/components/Employees_ChildComponents/EmployeeList.jsx
+++ src/components/Employees_ChildComponents/EmployeeList.jsx
@@ -1,15 +1,6 @@
-/* Source: https://stackoverflow.com/questions/36795819/when-should-i-use-curly-braces-for-es6-import
-Importing things from a module outside of curly braces is a default import. It doesn't matter
-what name youre assigning it when importing, because it always resolves to whatever the
-default export is. Importing things inside curly braces is a named import. It only works
-if the file you're importing from contains an import with the same name. In this case,
-the name matters 'cause you're importing a specific thing by it's export name. A module can
-have only one default export, but as many named exports as you'd like. */
 import React, { useState, useEffect } from 'react'
 import Employee from './Employee.jsx'
 import $ from 'jquery'
-
-// Source for useEffect logic: https://dev.to/sergioholgado/how-to-fetch-data-before-rendering-in-react-js-3750
 
 export default function EmployeeList() {
     const [employeeData, setEmployeeData] = useState([])
@@ -29,8 +20,7 @@ export default function EmployeeList() {
         })
     }, [])
     /* The second arg for useEffect() is an optional list of dependencies. If omitted,
-        useEffect() will re-run after every re-render of the component.
-    */
+        useEffect() will re-run after every re-render of the component. */
 
     return (
         <ul>
