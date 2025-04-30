5ac5203641528ffe392329da6ff73f4d53686d40
WilliamPrakash <willprakash@gmail.com>
2025-04-29 21:43:04 -0400

    Starting to implement newEmployee creation logic

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import EmployeeList from './Employees_ChildComponents/EmployeeList.jsx'
+import React, { useState } from 'react';
+import EmployeeList from './Employees_ChildComponents/EmployeeList.jsx';
+import NewEmployee from './Employees_ChildComponents/NewEmployee.jsx';
+import $ from 'jquery';
 
 export default function Employees() {
+    const [employee, setEmployee] = useState({'Name': null, 'Email': null, 'Occupation': null});
+
+    function createEmployee(name, email, occupation) {
+        let newEmployee = {
+            'Name': name,
+            'Email': email,
+            'Occupation': occupation
+        }
+        // Post
+        $.ajax({
+            type: 'POST',
+            url: '127.0.0.1:5215/api/Employees/CreateEmployee',
+            data: JSON.stringify(newEmployee),
+            dataType: 'JSON',
+            contentType: "application/json; charset=utf-8",
+            success: function(res) {
+                console.log(res);
+            }
+        });
+    }
+    
+
     return (
         <div>
             <h1>Employee</h1>
             <EmployeeList />
+            <br />
+            <NewEmployee setEmployeeCallback={setEmployee}/>
         </div>
     )
 }
---
+++ src/components/Employees_ChildComponents/NewEmployee.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NewEmployee(props) {
+
+
+    return (
+        <div>
+            <h2>Create New Employee</h2>
+            <form>
+
+            </form>
+        </div>
+    );
+}
