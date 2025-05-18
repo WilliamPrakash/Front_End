7ec7f35d5361e6bf836768e8c11eb8db4218a554
WilliamPrakash <willprakash@gmail.com>
2025-05-18 10:01:05 -0400

    Removed old create-react-app app

--- dir/bundle.js
+++ dir/bundle.js
@@ -51214,7 +51214,7 @@ var __webpack_exports__ = {};
   \*************************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* binding */ index)
+/* harmony export */   "default": () => (/* binding */ Index)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
@@ -51238,7 +51238,7 @@ __webpack_require__.r(__webpack_exports__);
 // Nested <Route>s inherit and add to the parent route
 // So the route to the page1 component would be /page1
 
-function index() {
+function Index() {
   return (
     /*#__PURE__*/
     // BrowserRouter only handles URL routing.
@@ -51260,7 +51260,7 @@ function index() {
 
 // container??
 var root = react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot(document.getElementById('root'));
-root.render(/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("index", null));
+root.render(/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(Index, null));
 })();
 
 /******/ })()
